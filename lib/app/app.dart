d4150e663680c7d81b1a6498f2e7a2ecc0bdf5b4
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-10-25 17:20:30 +0600

    Refactor ConnectXApp to utilize dynamic app title and debug banner based on app flavor

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../core/config/app_flavor.dart';
+import 'router/app_router.dart';
+import 'theme/app_theme.dart';
+
 
 class ConnectXApp extends StatelessWidget {
   const ConnectXApp({super.key});
@@ -17,13 +21,29 @@ class ConnectXApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp.router(
-        title: 'ConnectX',
-        debugShowCheckedModeBanner: false,
+        title: _getAppTitle(),
+        debugShowCheckedModeBanner: _showDebugBanner(),
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
         themeMode: ThemeMode.system,
         routerConfig: AppRouter.router,
       ),
     );
+  }
+
+  String _getAppTitle() {
+    try {
+      return AppFlavor.instance.values.appName;
+    } catch (e) {
+      return 'ConnectX';
+    }
+  }
+
+  bool _showDebugBanner() {
+    try {
+      return AppFlavor.instance.isDevelopment && AppFlavor.instance.values.showFlavorBanner;
+    } catch (e) {
+      return false;
+    }
   }
 }
